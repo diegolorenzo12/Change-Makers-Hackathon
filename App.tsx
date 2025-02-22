704ee4b30d8ecdf8a52b35bc2602157d452df533
diegolorenzo12 <diegolorenzovillalobos@gmail.com>
2025-02-22 10:22:31 -0800

    test

--- App.tsx
+++ App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { View, SafeAreaView, StatusBar, Text } from "react-native";
 import { Amplify } from "aws-amplify";
-import { Authenticator } from "@aws-amplify/ui-react-native";
 import outputs from "./amplify_outputs.json";
 import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
@@ -24,28 +23,23 @@ signInWithRedirect({
 const App = () => {
     return (
         <View className="h-full bg-white">
-            {/* <Authenticator.Provider>
-                <Authenticator
-                > */}
-                    <StatusBar barStyle="light-content" backgroundColor="white" />
-                    <SafeAreaView className="h-full">
-                        <NavigationContainer>
-                            <Stack.Navigator>
-                                <Stack.Screen
-                                    name="Home"
-                                    component={HomeScreen}
-                                    options={{ title: 'Photo Feed' }}
-                                />
-                                <Stack.Screen
-                                    name="Upload"
-                                    component={UploadScreen}
-                                    options={{ title: 'Upload Photo' }}
-                                />
-                            </Stack.Navigator>
-                        </NavigationContainer>
-                    </SafeAreaView>
-                {/* </Authenticator>
-            </Authenticator.Provider> */}
+            <StatusBar barStyle="light-content" backgroundColor="white" />
+            <SafeAreaView className="h-full">
+                <NavigationContainer>
+                    <Stack.Navigator>
+                        <Stack.Screen
+                            name="Home"
+                            component={HomeScreen}
+                            options={{ title: 'Photo Feed' }}
+                        />
+                        <Stack.Screen
+                            name="Upload"
+                            component={UploadScreen}
+                            options={{ title: 'Upload Photo' }}
+                        />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </SafeAreaView>
         </View>
     );
 };
